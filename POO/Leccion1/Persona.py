db958bb206b9f7e569cb24cdd1705d914252936a
DF490 <diego.mc09@outlook.com>
2022-03-30 17:21:01 -0500

    Agregando setter y getters en Persona.py

--- POO/Leccion1/Persona.py
+++ POO/Leccion1/Persona.py
@@ -1,11 +1,22 @@
 class Persona:
     def __init__(self, nombre, apellido, edad):
-        self.nombre = nombre
+        self._nombre = nombre
         self.apellido = apellido
         self.edad = edad
+
+    ''' Getter '''
+    @property
+    def nombre(self):
+        return self._nombre
+
+    ''' Setter '''
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
 
     def mostrar_detalle(self):
         print(f'{persona1.nombre} {persona1.apellido} {persona1.edad}')
 
 persona1 = Persona('Alan', 'Grill', '38')
-persona1.mostrar_detalle()
+persona1.nombre = 'Alan Britho'
+print(persona1.nombre)
